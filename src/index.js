480c2aff37495f5cbb02da788801891ff66ce8e5
Prizmor <yy3980075@gmail.com>
2022-11-27 01:54:34 +0300

    asd
    a

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const client = new Client({
 
 const TOKEN = process.env.BOT_TOKEN;
 
-const byvaet = ["бывает", ",sdftn", ",SDFTN", "БЫВАЕТ", "бывет"];
+const byvaet = ["бывает", ",sdftn", ",SDFTN", "БЫВАЕТ", "бывет", "Бывет"];
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -20,7 +20,7 @@ client.on("messageCreate", async (message) => {
     if (message.author.discriminator === client.user.discriminator) return;
 
     for (let i = 0; i < byvaet.length; i++) {
-        if (message.content.indexOf(byvaet[i]) !== -1) {
+        if (message.content.toLocaleUpperCase().indexOf(byvaet[i].toLocaleUpperCase()) !== -1) {
             return message.reply("https://cdn.discordapp.com/attachments/1014783512731193356/1046163144390746133/image.png");
         }
     }
